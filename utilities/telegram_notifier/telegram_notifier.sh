57dff48b12a6e360423eea68008d8ce5aefce479
megahomyak <g.megahomyak@gmail.com>
2025-11-29 17:50:11 +0500

    Found my mistake! Whoopsie!

--- utilities/telegram_notifier/telegram_notifier.sh
+++ utilities/telegram_notifier/telegram_notifier.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source env
+. ./env
 while :; do
     TEXT="$(curl --http0.9 -s -S -f -d "$(printf '%s\n%s\n%s\n%s\n%s\n.' "$( (stat -c %s "$CHATTER_ROOMNAME" || stat -f %z "$CHATTER_ROOMNAME") 2>/dev/null || printf 0)" "$CHATTER_USERNAME" "$CHATTER_PASSWORD" "$CHATTER_ROOMNAME" "" "$CHATTER_URL" | tee -a "$CHATTER_ROOMNAME")"
     if [ "$TEXT" != "" ]; then
