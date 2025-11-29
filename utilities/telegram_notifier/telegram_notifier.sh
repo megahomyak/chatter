be283f63bfe60e6c012f07ceea9c78ea2c699997
megahomyak <g.megahomyak@gmail.com>
2025-11-29 17:12:50 +0500

    Sending whole messages + fixes

--- utilities/telegram_notifier/telegram_notifier.sh
+++ utilities/telegram_notifier/telegram_notifier.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 source env
 while :; do
-    if [ "$(curl --http0.9 -s -S -f -d "$(printf '%s\n%s\n%s\n%s\n%s\n.' "$( (stat -c %s "$CHATTER_ROOMNAME" || stat -f %z "$CHATTER_ROOMNAME") 2>/dev/null || printf 0)" "$CHATTER_USERNAME" "$CHATTER_PASSWORD" "$CHATTER_ROOMNAME" "" "$CHATTER_URL" | tee -a "$CHATTER_ROOMNAME")" != "" ]; then
-        curl -H 'Content-Type: application/json' -d "{\"chat_id\": \"$TELEGRAM_CHAT_ID\", \"text\": \"New Chatter messages in $CHATTER_ROOMNAME on $CHATTER_URL\"}" "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+    TEXT="$(curl --http0.9 -s -S -f -d "$(printf '%s\n%s\n%s\n%s\n%s\n.' "$( (stat -c %s "$CHATTER_ROOMNAME" || stat -f %z "$CHATTER_ROOMNAME") 2>/dev/null || printf 0)" "$CHATTER_USERNAME" "$CHATTER_PASSWORD" "$CHATTER_ROOMNAME" "" "$CHATTER_URL" | tee -a "$CHATTER_ROOMNAME")"
+    if [ "$TEXT" != "" ]; then
+        TEXT="$(printf '%s @ %s :\n\n%s' "$CHATTER_ROOMNAME" "$CHATTER_URL" "$TEXT")"
+        PARAMETERS="$(jq -n --arg text "$TEXT" --arg chat_id "$TELEGRAM_CHAT_ID" '{text: $text, chat_id: $chat_id}')"
+        curl -H 'Content-Type: application/json' -d "$PARAMETERS" "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
     fi
     sleep 10
 done
