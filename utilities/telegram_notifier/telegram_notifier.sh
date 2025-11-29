7524d970461878cd32baa6d12a6f468ec8c004dd
megahomyak <g.megahomyak@gmail.com>
2025-11-29 18:06:50 +0500

    Another syntax error fix

--- utilities/telegram_notifier/telegram_notifier.sh
+++ utilities/telegram_notifier/telegram_notifier.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . ./env
 while : ; do
-    TEXT="$(curl --http0.9 -s -S -f -d "$(printf '%s\n%s\n%s\n%s\n%s\n.' "$( (stat -c %s "$CHATTER_ROOMNAME" || stat -f %z "$CHATTER_ROOMNAME") 2>/dev/null || printf 0)" "$CHATTER_USERNAME" "$CHATTER_PASSWORD" "$CHATTER_ROOMNAME" "" "$CHATTER_URL" | tee -a "$CHATTER_ROOMNAME")")"
+    TEXT="$(curl --http0.9 -s -S -f -d "$(printf '%s\n%s\n%s\n%s\n%s\n.' "$( (stat -c %s "$CHATTER_ROOMNAME" || stat -f %z "$CHATTER_ROOMNAME") 2>/dev/null || printf 0)" "$CHATTER_USERNAME" "$CHATTER_PASSWORD" "$CHATTER_ROOMNAME" "")" "$CHATTER_URL" | tee -a "$CHATTER_ROOMNAME")"
     if [ "$TEXT" != "" ]; then
         TEXT="$(printf '%s @ %s :\n\n%s' "$CHATTER_ROOMNAME" "$CHATTER_URL" "$TEXT")"
         PARAMETERS="$(jq -n --arg text "$TEXT" --arg chat_id "$TELEGRAM_CHAT_ID" '{text: $text, chat_id: $chat_id}')"
