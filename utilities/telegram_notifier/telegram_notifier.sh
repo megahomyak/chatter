d07c01f67ba2274acf98d9381e7f04b580c56f04
megahomyak <g.megahomyak@gmail.com>
2025-11-29 17:15:55 +0500

    I hope this is not gonna fail

--- utilities/telegram_notifier/telegram_notifier.sh
+++ utilities/telegram_notifier/telegram_notifier.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 source env
 while :; do
     TEXT="$(curl --http0.9 -s -S -f -d "$(printf '%s\n%s\n%s\n%s\n%s\n.' "$( (stat -c %s "$CHATTER_ROOMNAME" || stat -f %z "$CHATTER_ROOMNAME") 2>/dev/null || printf 0)" "$CHATTER_USERNAME" "$CHATTER_PASSWORD" "$CHATTER_ROOMNAME" "" "$CHATTER_URL" | tee -a "$CHATTER_ROOMNAME")"
